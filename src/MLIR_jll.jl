7d497fd5ce0b6d8ebea864a00b916be0a28672e0
jlbuild <juliabuildbot@gmail.com>
2021-07-18 10:27:55 +0000

    MLIR_jll build 12.0.1+0

---
+++ src/MLIR_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule MLIR_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("MLIR")
+JLLWrappers.@generate_main_file("MLIR", UUID("a70bccb4-a5c0-5e2e-a329-e731972457e8"))
+end  # module MLIR_jll
